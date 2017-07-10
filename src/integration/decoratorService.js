651163bbf7513b5c2bc20716445aaea7c00c401b
Justin Robison <jrobison151@gmail.com>
2017-07-10 06:14:05 -0400

    refactor(event): create appropriate event structure
    Previous event incorreclty sent payload to a topic named as an event (past tense). Refactor was needed to create an appropriate
    event structure with an eventType (past tense) and said event is now published to a message topic for the domain

--- src/integration/decoratorService.js
+++ src/integration/decoratorService.js
@@ -1,3 +1,4 @@
+import uuid from 'uuid';
 
 export default class DecoratorService {
 
@@ -8,7 +9,17 @@ export default class DecoratorService {
 
   decorateTicker(ticker) {
 
-    this.publisher.publish('UNDECORATED_TICKER_RECEIVED', ticker);
+    const timestamp = new Date();
+
+    const event = {
+      id: uuid.v4(),
+      createdTime: timestamp.getTime(),
+      createdTimePretty: timestamp.toUTCString(),
+      eventType: 'UNDECORATED_TICKER_RECEIVED',
+      payload: ticker,
+    };
+
+    this.publisher.publish('DECORATION', event);
   }
 }
 
--- test/unit/integration/decoratorServiceTest.js
+++ test/unit/integration/decoratorServiceTest.js
@@ -7,6 +7,8 @@ describe('Decorator Service Tests', () => {
 
   describe('when decorating a ticker', () => {
 
+    const MAX_MILLISECONDS_IN_ONE_SECOND = 1000;
+
     const tickerToDecorate = {
       id: 'aadsfasfs',
       ticker: 'GOOG',
@@ -15,6 +17,7 @@ describe('Decorator Service Tests', () => {
     };
 
     let redisClientSpy;
+    let lastEvent;
 
     beforeEach(() => {
 
@@ -23,16 +26,42 @@ describe('Decorator Service Tests', () => {
 
       const decoratorService = new DecoratorService(redisStub);
       decoratorService.decorateTicker(tickerToDecorate);
+
+      lastEvent = redisClientSpy.getLastTickerEvent();
     });
 
-    it('emits the ticker as an event', () => {
+    it('emits an event with the event type set', () => {
 
-      expect(redisClientSpy.getLastTickerEvent()).to.deep.equal(tickerToDecorate);
+      expect(lastEvent.eventType).to.equal('UNDECORATED_TICKER_RECEIVED');
+    });
+
+    it('emits an the event with the ticker as the payload', () => {
+
+      expect(lastEvent.payload).to.deep.equal(tickerToDecorate);
+    });
+
+    it('emits the event with a unique id', () => {
+
+      expect(lastEvent.id).to.match(/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{10}/);
+    });
+
+    it('emits the event with a creation timestamp', () => {
+
+      // eslint-disable-next-line no-unused-expressions
+      expect(lastEvent.createdTime).to.not.be.undefined;
+    });
+
+    it('emits the event with a human readable creation timestamp', () => {
+
+      const prettyTimeAsTime = Date.parse(lastEvent.createdTimePretty);
+      const timeDifference = lastEvent.createdTime - prettyTimeAsTime;
+
+      expect(timeDifference).to.be.below(MAX_MILLISECONDS_IN_ONE_SECOND);
     });
 
     it('emits the event on the right channel', () => {
 
-      expect(redisClientSpy.getLastChannelPublishedTo()).to.equal('UNDECORATED_TICKER_RECEIVED');
+      expect(redisClientSpy.getLastChannelPublishedTo()).to.equal('DECORATION');
     });
   });
 });
